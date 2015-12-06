675cc45e768d124aa8fb13b4d73626904ee5987e
Roger Stringer <roger@freekrai.net>
2015-12-05 23:20:10 -0800

    dynamically load mondernizr and flybase.js

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ This will push real-time events to your dashboard so you can see visitors on you
 If you want to store user data for historical purposes, then you can also set data to `push` to your flybase account.
 
 Currently, this is entirely real-time event driven, so no data is stored.
+
+
+### Updates
+
+This version has been updated so that `modernizer` and `flybase.js` are included dynamically inside the `flyalytics.js` file.
+
+This means that all you have to do for tracking is:
+
+```javascript
+<script src="flyalytics.js">
+```
+
+And the rest is handled for you.
--- flyalytics.js
+++ flyalytics.js
@@ -1,9 +1,45 @@
-var flybase = new Flybase("YOUR-FLYBASE-API-KEY", "flyalytics", "stats");
-var data = {
-	url: window.location.href,
-	webgl: Modernizr.webgl,
-	touch: Modernizr.touch,
-	video: Modernizr.video,
-	websocket: Modernizr.websockets
-};
-flybase.trigger( 'client-data', data );
+/*
+	include() loads scripts asynchronously...
+*/
+function include(url,success) {
+	var head = document.getElementsByTagName("head")[0], done = false;
+	var script = document.createElement("script");
+	script.src = url;
+	// Attach handlers for all browsers
+	script.onload = script.onreadystatechange = function(){
+		if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") ) {
+			done = true;
+			if (typeof success === 'function') success();
+		}
+	};
+	head.appendChild(script);
+}
+
+//	load modernizer first...
+include('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js',function(){
+	//	now load flybase.js...
+	include('https://cdn.flybase.io/flybase.js',function(){
+		//	now initialize and track...
+		flyalytics_init();
+	});
+});
+
+/*
+Once we've included modernizr and flybase, then we call this function to begin tracking.	
+*/
+function flyalytics_init(){
+	var flybase = new Flybase("YOUR-FLYBASE-API-KEY", "flyalytics", "stats");
+	var data = {
+		url: window.location.href,
+		webgl: Modernizr.webgl,
+		touch: Modernizr.touch,
+		video: Modernizr.video,
+		websocket: Modernizr.websockets
+	};
+	flybase.trigger( 'client-data', data );
+}
+
+// you can use flyalytics_event to push custom events to the `client-event` listener.
+function flyatics_event(data){
+	flybase.trigger( 'client-event', data );
+}
